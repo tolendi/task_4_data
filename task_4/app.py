12af505d2105cfb9a3a5aa7c1d0869f46f25fecb
tolendi <95737085+tolendi@users.noreply.github.com>
2025-12-02 01:05:49 +0500

    Update app.py

--- task_4/app.py
+++ task_4/app.py
@@ -1,16 +1,5 @@
-import streamlit as st
-import json
 import os
+import streamlit as st
 
-st.title("Test Dashboard")
-
-folders = ["DATA1", "DATA2", "DATA3"]
-
-for folder in folders:
-    st.header(folder)
-    path = f"{folder}_results.json"
-    st.write("Path exists?", os.path.exists(path))
-    if os.path.exists(path):
-        with open(path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-        st.write(data)
+st.write("Current working directory:", os.getcwd())
+st.write("Files in directory:", os.listdir())
